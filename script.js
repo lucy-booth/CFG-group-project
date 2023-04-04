112b765bc67cb4a028f519e5b29d340491bc6b53
Lucy Booth <lucy.booth89@gmail.com>
2023-04-04 22:02:30 +0100

    added button for future new html pg, added some js

---
+++ script.js
@@ -0,0 +1,13 @@
+const location1 = {
+    city: "Paris",
+    country: "France",
+    img: "paris.webp"
+}
+
+const location2 = {
+    city: "Test",
+    country: "Testttt",
+    img: "paris.webp"
+}
+
+const locationsArray = [location1, location2];
